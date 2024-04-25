e81b7190f721ec33f8d532b61865b649fb4f3817
Victo-1940381 <1940381@carrefour.cegepvicto.ca>
2024-04-25 09:26:01 -0400

    debut projet

---
+++ exercise avion/Siège/Siège.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblio_classe_avion
+{
+    internal class Siège
+    {
+    }
+}
