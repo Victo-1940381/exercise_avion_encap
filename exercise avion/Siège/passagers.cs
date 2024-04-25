126f693b747dea7e7426898bd10a8ba55872fb66
Victo-1940381 <1940381@carrefour.cegepvicto.ca>
2024-04-25 09:58:30 -0400

    ajout classe passagers

---
+++ exercise avion/Siège/passagers.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblio_classe_avion
+{
+    /// <summary>
+    /// représente un passager d'un avion
+    /// </summary>
+    public class passagers
+    {
+    }
+}
