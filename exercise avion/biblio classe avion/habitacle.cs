e589d2d445242ef4f4544baf3c4b3d73a93ff1d8
Victo-1940381 <1940381@carrefour.cegepvicto.ca>
2024-04-29 11:09:29 -0400

    ajout de la classe habitacle

---
+++ exercise avion/biblio classe avion/habitacle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace biblio_classe_avion
+{
+    /// <summary>
+    /// représente un habitacle d'un avion avec tout les siege
+    /// </summary>
+    public class habitacle : Ranger
+    {
+        /// <summary>
+        /// constrcuteur d'un habitacle
+        /// </summary>
+        /// <param name="classe">la classe d'un siege</param>
+        /// <param name="lettre_siege_section">la lettre d'un siege</param>
+        /// <param name="num_siege">le numero du siege</param>
+        public habitacle(string classe, char lettre_siege_section, int num_siege) : base(classe, lettre_siege_section, num_siege)
+        {
+        }
+    }
+}
