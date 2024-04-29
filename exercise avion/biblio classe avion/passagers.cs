3510394ab578229dd9a71b3827e8830b99ad893b
Victo-1940381 <1940381@carrefour.cegepvicto.ca>
2024-04-29 10:53:54 -0400

    correction de la classe passagers

--- exercise avion/biblio classe avion/passagers.cs
+++ exercise avion/biblio classe avion/passagers.cs
@@ -12,16 +12,19 @@ namespace biblio_classe_avion
     public class passagers
     {
         private string _nom;
-        private string _cordonné_siege;
+        private int _numéro_siege;
+        private char _lettre_siege;
         /// <summary>
         /// contructeur d'un passager
         /// </summary>
         /// <param name="nom">le nom du passager</param>
-        /// <param name="cordonné_siege">la coordonné de son siege</param>
-        public passagers(string nom, string cordonné_siege)
+        /// <param name="numéro_siege ">le numrero de son siege </param>
+        /// <param name="lettre_siege">la lettre de son siege </param>
+        public passagers(string nom, int numéro_siege,char lettre_siege)
         {
             this._nom = nom;
-            this._cordonné_siege = cordonné_siege;
+            this._numéro_siege = numéro_siege;
+            this._lettre_siege = lettre_siege;
         }
     }
 }
