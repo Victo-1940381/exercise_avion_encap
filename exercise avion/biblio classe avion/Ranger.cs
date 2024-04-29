72204b9ebf59dfb7a286d4125dccbc4dd5909766
Victo-1940381 <1940381@carrefour.cegepvicto.ca>
2024-04-29 11:05:22 -0400

    ajout des summary

--- exercise avion/biblio classe avion/Ranger.cs
+++ exercise avion/biblio classe avion/Ranger.cs
@@ -13,6 +13,12 @@ namespace biblio_classe_avion
     public class Ranger : Section
     {    
         private int _num_siege;
+        /// <summary>
+        /// constructeur d'une ranger
+        /// </summary>
+        /// <param name="classe">la classe du siege</param>
+        /// <param name="lettre_siege_section">la lettre du siege</param>
+        /// <param name="num_siege">le numero du siège</param>
         public Ranger(string classe,char lettre_siege_section,int num_siege) : base(classe,lettre_siege_section)
         {
             this._num_siege = num_siege;
